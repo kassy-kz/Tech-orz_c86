cd4cb4d90de624d8996f32a22aa17a326d5ad26b
kashimoto <kashimoto.kazutoshi@gmail.com>
2014-08-11 22:33:54 +0900

    arduinoスケッチを2.2章相当に更新

--- C86_Arduino/C86_Arduino.ino
+++ C86_Arduino/C86_Arduino.ino
@@ -1,17 +1,14 @@
 #include <AquesTalk.h>
-#include <Wire.h>  // I2CライブラリAquesTalkライブラリ内部で使用するので定義必要
+#include <Wire.h> 
 
-// Hello Talk - AquesTalk pico LSIで「こんにちは」を繰り返し発声する
+AquesTalk atp; 
 
-AquesTalk atp;  //インスタンス定義 変数名は任意
-
-void setup()
-{
+void setup(){
 }
 
-void loop()
-{
-	atp.Synthe("konnnichiwa.");  //引数に指定のテキストを発声
+void loop(){
+  // こんにちは と発声（無限ループ）
+  atp.Synthe("konnnichiwa.");  
 }
 
 
