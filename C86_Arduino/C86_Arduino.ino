c88413cbef25bc2c54b05df04ceb2302d4e23644
kashimoto <kashimoto.kazutoshi@gmail.com>
2014-08-12 23:29:22 +0900

    BLESerialでスマホと通信できるようにスケッチ変更

--- C86_Arduino/C86_Arduino.ino
+++ C86_Arduino/C86_Arduino.ino
@@ -3,12 +3,37 @@
 
 AquesTalk atp; 
 
+// BLESerialの受信データ
+int rcvData = 0;
+
+
 void setup(){
+  // デバッグシリアル
+  Serial.begin(9600);
+
+  // for BLESerial
+  // Serial2を使用する
+  pinMode(16, OUTPUT);
+  // シリアル通信セットアップ
+  Serial2.begin(9600);
+
 }
 
 void loop(){
   // こんにちは と発声（無限ループ）
   atp.Synthe("konnnichiwa.");  
+  
+  // BLESerial受信処理
+  char buf[20];
+  if(Serial2.available() > 0){
+    // シリアルから読み取り
+    rcvData = Serial2.readBytes(buf, 20);
+    Serial.print("BLESerial receive ");
+    Serial.println(buf);
+
+    // BLESerialで返答を送信する
+    Serial2.write("success receive");
+  }
 }
 
 
